c6ac1daf36df85cbcea69699ac634aa1200e5b75
christoph <chaslinger@student.tgm.ac.at>
2018-10-12 14:09:43 +0200

    ShiftCipher class + testClass implementation of shiftCipher

--- src/haslingerCipher/testClass.java
+++ src/haslingerCipher/testClass.java
@@ -1,16 +1,31 @@
 package haslingerCipher;
-/** This class tests the encrypt and decrypt function of MonoAlphabeticCipher
+/** This class tests the encrypt and decrypt function of SubstitutionCipher and ShiftCipher
  * 
  * @author Christoph Haslinger
  * @version 11-10-2018
  */
 public class testClass {
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
+		System.out.println("Testing SubstitutionCipher");
 		try {
 			SubstitutionCipher s = new SubstitutionCipher("qwertzuiopüäölkjhgfdsayxcvbnßm");
 			String text = s.encrypt("abcdefghijklmnopqrstuvwxyzäöüß");
-			System.out.println(text);
-			System.out.println(s.decrypt(text));
+			System.out.println("\tencrypted text:");
+			System.out.println("\t\t" + text);
+			System.out.println("\tdecrypted text:");
+			System.out.println("\t\t" + s.decrypt(text));
+		}catch(AlphaException e) {
+			System.out.println(e.toString());
+		}
+		System.out.println("//////////////////////////////////////////////////");
+		System.out.println("Testing ShiftCipher");
+		try {
+			ShiftCipher s = new ShiftCipher(-33);
+			String text = s.encrypt("abcdefghijklmnopqrstuvwxyzäöüß");
+			System.out.println("\tencrypted text:");
+			System.out.println("\t\t" + text);
+			System.out.println("\tdecrypted text:");
+			System.out.println("\t\t" + s.decrypt(text));
 		}catch(AlphaException e) {
 			System.out.println(e.toString());
 		}
